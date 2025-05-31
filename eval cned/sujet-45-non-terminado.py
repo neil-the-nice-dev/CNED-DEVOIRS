fe7cbe2ddc597ca590bd7dba38b2781439fe8d6f
Pipoune974 <130435869+Pipoune974@users.noreply.github.com>
2025-05-31 11:16:15 +0400

    up

--- eval cned/sujet-45-non-terminado.py
+++ eval cned/sujet-45-non-terminado.py
@@ -8,6 +8,17 @@ def correspond(mot, mot_troue):
     else :
         return False    
     
+def est_cyclique(plan):
+    '''Prend en paramètre un dictionnaire `plan` correspondant à un plan d'envoi de messages (ici entre les personnes A, B, C, D, E, F).
+    Renvoie True si le plan d'envoi de messages est cyclique et False sinon.'''
+    expediteur = 'A'
+    destinataire = plan[expediteur]
+    nb_destinataires = 1
+    while destinataire != expediteur: 
+        destinataire = plan[destinataire]
+        nb_destinataires = nb_destinataires+1 
+    return nb_destinataires == len(plan)
+        
 if __name__ == '__main__':
-    print(correspond('AUTO', '*UT*'))    
+    print(est_cyclique({'A':'E','F':'C','C':'D','E':'B','B':'F','D':'A'}))    
         
